895bdc6ad34595e06c58e2f06dbc34a5279812e5
Amanda Herschleb <amanda.herschleb@gmail.com>
2018-01-11 13:57:01 -0700

    travis and heroku initial setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = {app};
